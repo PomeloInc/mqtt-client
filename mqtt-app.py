dfa80af62de349c55fc10a5751711fab496727c8
pomelo <ben.humml.tech@gmail.com>
2022-02-06 14:43:55 +0000

    Initial commit. Added mqtt-app.py and launch.json

---
+++ mqtt-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from time import sleep
+
+def main():
+    """ Main program """
+    
+    for i in range(0, 100):
+        print('Hello World')
+        sleep(0.5)
+    return 0
+
+if __name__ == "__main__":
+    main()
