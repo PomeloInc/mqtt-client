9d67b9da8a5e6172da271614eeb9e3a735ba8956
pomelo <ben.humml.tech@gmail.com>
2022-02-08 20:48:31 +0000

    updated MQTT app

--- mqtt-app.py
+++ mqtt-app.py
@@ -1,15 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
 from time import sleep
+import paho.mqtt.client as mqtt
+import random
+
+
+# def on_connect(client, userdata, flags, rc):
+# def publish(client):
+# def subscribe(client: mqtt_client):
+# def on_message(client, userdata, msg):
+# def on_message(client, userdata, msg):
 
 def main():
     """ Main program """
-    
-    for i in range(0, 100):
-        print('Hello World')
-        sleep(0.5)
+
+   
+    broker_url = "test.mosquitto.org"
+    broker_port = 1883  
+    client_id = f'python-mqtt-{random.randint(0, 1000)}'
+    client = mqtt.Client(client_id=client_id)
+    client.connect(broker_url, broker_port)
+    topic = "/pomelo/client_1"
+    client.publish(topic="TestingTopic", payload="TestingPayload", qos=0, retain=False)
+
+    try:
+        for i in range(0, 5):
+            client.publish(topic, "Hello from BBB!")
+            print('Hello World')
+            sleep(0.5)
+    except BaseException as berr:
+        print(berr)
+
+    client.disconnect()
+
     return 0
 
 if __name__ == "__main__":
