f777614284493a1e0777db15e2866bcd9162e331
Benedikt Humml <ben.humml.tech@gmail.com>
2022-03-24 02:24:09 +0100

    Bugfixes PomeloClient

--- pom_classes/PomeloClient.py
+++ pom_classes/PomeloClient.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import json
+import socket
 import paho.mqtt.client as mqtt
+from time import sleep
+import os
+
+def read_config(path):
+        '''TODO: documentation'''
+        try:
+            with open(path, encoding='UTF-8') as file:
+                cfg_dict = json.load(file)
+
+            config = PomeloClient.Config(
+                broker=cfg_dict['broker']['url'],
+                port=cfg_dict['broker']['port'],
+                id=cfg_dict['client_id'],
+                topics=cfg_dict['topics']
+            )
+            return config
+        except BaseException as berr:
+            return berr
+
+def on_connect(client, userdata, flags, rc):
+        '''TODO: documentation'''
+        print("Connected with result code "+str(rc))
+        client.publish("pomelo/registration", "Hello from " + os.getlogin() + "@" + socket.gethostname())
+
+def on_publish(client, userdata, msg):
+    '''TODO: documentation'''
+    print("DEBUG: published message")
 
 class PomeloClient:
     # TODO: add inheritence logic, i.e. it should be possible to create sub-classes a'la "webapp-client, build-client etcpp."
-
-    def __init__(self, id=0, cfg=None):
+    def __init__(self, cfg=None):
         """TODO: document"""
         try:
             self.id = id
-            self.cfg = PomeloClient.Config(self.read_config(cfg))
+            self.cfg = read_config(cfg)
             self.client = mqtt.Client()
+            self.client.on_connect = on_connect
+            self.client.on_publish = on_publish
         except BaseException as berr:
             return berr
 
@@ -19,21 +48,6 @@ class PomeloClient:
         """TODO: document"""
         return self.id
         
-    def read_config(path):
-        '''TODO: documentation'''
-        try:
-            with open(path, encoding='UTF-8') as file:
-                cfg_dict = json.load(file)
-
-            config = PomeloClient.Config(
-                id=cfg_dict['client_id'],
-                port=cfg_dict['broker']['port'],
-                broker_address=cfg_dict['broker']['url'],
-                topics=cfg_dict['topics']
-            )
-            return config
-        except BaseException as berr:
-            return berr
     
     def connect(self):
         """TODO: document"""
@@ -43,20 +57,30 @@ class PomeloClient:
         except BaseException as berr:
             return berr
 
-    def disconnect():
+    def disconnect(self):
         """TODO: document"""
-        return "disconnected"
+        try:
+            self.client.disconnect()
+            self.client.loop_stop()
+        except BaseException as berr:
+            return berr
     
-    def loop():
+    def loop(self, i):
         """TODO: document"""
-        return "loop"
+        try:
+            print(self.cfg.topics['data'])
+            self.client.publish(self.cfg.topics['data'], i)
+            sleep(0.5)
+        except BaseException as berr:
+            return berr
 
     class Config:
-        # TODO:
-        def __init__(self, broker, port, client_id, topics, client_secret=None):
+        """ TODO: document """
+
+        def __init__(self, broker, port, id, topics, client_secret=None):
             """TODO: document"""
             try:
-                self.id = client_id
+                self.id = id
                 self.boker = broker
                 self.port = port
                 self.topics = topics
