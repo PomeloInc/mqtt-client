702ca1435a7e4ad7e328fefb0354e764c5d84903
Benedikt Humml <ben.humml.tech@gmail.com>
2022-03-24 20:58:52 +0100

    Added Pomelo auth client

---
+++ pom_classes/PomeloAuthenticationClient.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+TODO: DOCUMENT AND COPYRIGHT/LICENSE
+"""
+
+import PomeloClient
+
+class PomeloAuthenticationClient(PomeloClient):
+
+    def __init__(self, cfg=None):
+        super().__init__(cfg)
+
+    
