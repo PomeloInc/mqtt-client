461f6314688b03ca7a7661221265b44cd10f757f
Benedikt Humml <ben.humml.tech@gmail.com>
2022-03-24 01:14:17 +0100

    First draft client class

--- classes/pomelo-mqtt-client.py
+++ classes/pomelo-mqtt-client.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import json
+from lib2to3.pytree import Base
+from pkgutil import read_code
+from tkinter.messagebox import NO
+import paho.mqtt.client as mqtt
 
 class Pomelo_Client:
     # TODO: add inheritence logic, i.e. it should be possible to create sub-classes a'la "webapp-client, build-client etcpp."
 
-    def __init__(id=0):
+    def __init__(self, id=0, cfg=None):
         """TODO: document"""
-        self.id = id
+        try:
+            self.id = id
+            self.cfg = Pomelo_Client.Config(self.read_config(cfg))
+            self.client = mqtt.Client()
+        except BaseException as berr:
+            return berr
 
     def __str__(self):
-        return self.id
-
-
-    def connect():
         """TODO: document"""
-        return "connected"
+        return self.id
+        
+    def read_config(path):
+        '''TODO: documentation'''
+        try:
+            with open(path, encoding='UTF-8') as file:
+                cfg_dict = json.load(file)
+
+            config = Pomelo_Client.Config(
+                id=cfg_dict['client_id'],
+                port=cfg_dict['broker']['port'],
+                broker_address=cfg_dict['broker']['url'],
+                topics=cfg_dict['topics']
+            )
+            return config
+        except BaseException as berr:
+            return berr
+    
+    def connect(self):
+        """TODO: document"""
+        try:
+            self.client.connect(self.cfg.boker, self.cfg.port)
+            self.client.loop_start()
+        except BaseException as berr:
+            return berr
 
     def disconnect():
         """TODO: document"""
@@ -24,4 +54,19 @@ class Pomelo_Client:
         """TODO: document"""
         return "loop"
 
-    
+    class Config:
+        # TODO:
+        def __init__(self, broker, port, client_id, topics, client_secret=None):
+            """TODO: document"""
+            try:
+                self.id = client_id
+                self.boker = broker
+                self.port = port
+                self.topics = topics
+                self.client_secret = client_secret
+            except BaseException as berr:
+                return berr
+
+        def __str__(self):
+            """TODO: document"""
+            return self.id + self.broker_addr
